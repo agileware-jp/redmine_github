11217dab1805e77d68584cd218b76924332eaf51
Sho DOUHASHI <douhashi@gmail.com>
2019-03-09 03:57:50 +0900

    git clone with ssh_key

--- lib/redmine_github/scm/adapters/github_adapter.rb
+++ lib/redmine_github/scm/adapters/github_adapter.rb
@@ -11,8 +11,24 @@ module RedmineGithub::Scm::Adapters
     def bare_clone
       return if Dir.exists?(root_url)
 
-      cmd_args = %W[clone --bare #{url} #{root_url}]
-      git_cmd(cmd_args)
+      configure_ssh_key do 
+        cmd_args = %W[clone --bare #{url} #{root_url}]
+        git_cmd(cmd_args)
+      end
+    end
+
+    def configure_ssh_key(&block)
+      if User.current.ssh_key.present? && User.current.ssh_key.private_key.present?
+        Tempfile.create('id_rsa') do |f|
+          f.write User.current.ssh_key.private_key
+
+          shellout("git config core.sshCommand \"ssh -i #{f.path} -F /dev/null\"")
+
+          yield
+        end
+      else
+        yield
+      end
     end
   end
 end
