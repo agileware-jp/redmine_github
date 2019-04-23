500cf09476b7d41948c234fb7ed551ec418e45ce
Stoyan Zhekov <zh@zhware.net>
2019-04-23 17:36:24 +0900

    Add PR URL field to the DB (#7)

--- db/migrate/002_create_pull_requests.rb
+++ db/migrate/002_create_pull_requests.rb
@@ -2,7 +2,7 @@ class CreatePullRequests < RedmineGithub::Migration::MIGRATION_CLASS
   def change
     create_table :pull_requests do |t|
       t.belongs_to :issue, index: true, foreign_key: true
-      t.boolean :merged, null: false, default: false
+      t.string :url, null: false
 
       t.timestamps null: false
     end
