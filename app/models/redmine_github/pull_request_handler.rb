f358fd3bc25059d505bea3067f8633d756bfe7e4
fkdkent <fkdkent@agileware.jp>
2019-04-24 17:50:06 +0900

    Parse an issue ID from webhook payloads (#13)
    * Add TargetRubyVersion
    
    * Implement extract_issue_id
    
    * Remove needless specs
    
    * Add head.ref (but it is not used)

--- app/models/redmine_github/pull_request_handler.rb
+++ app/models/redmine_github/pull_request_handler.rb
@@ -4,7 +4,13 @@ module RedmineGithub
 
     def handle(payload)
       PullRequest.create(url: payload.dig('pull_request', 'html_url'))
-      nil
+    end
+
+    def extract_issue_id(branch_name)
+      match = branch_name.to_s.match(/@(\d+)/)
+      return nil unless match
+
+      match.captures[0].to_i
     end
   end
 end
--- spec/models/redmine_github/pull_request_handler_spec.rb
+++ spec/models/redmine_github/pull_request_handler_spec.rb
@@ -4,14 +4,17 @@ RSpec.describe RedmineGithub::PullRequestHandler do
   describe '.handle' do
     subject { RedmineGithub::PullRequestHandler.handle(payload) }
 
-    let(:payload) { {} }
-
-    it { is_expected.to be_nil }
-
     context 'action is "opened"' do
-      let(:payload) {
-        { 'pull_request' => { 'html_url' => url } }
-      }
+      let(:payload) do
+        {
+          'pull_request' => {
+            'html_url' => url,
+            'head' => {
+              'ref' => 'feature/@1234-my_first_pr'
+            }
+          }
+        }
+      end
       let(:url) { 'https://github.com/company/repo/pull/1' }
 
       it { expect { subject }.to change(PullRequest, :count).by(1) }
@@ -20,6 +23,20 @@ RSpec.describe RedmineGithub::PullRequestHandler do
         subject
         expect(PullRequest.exists?(url: url)).to be true
       end
+    end
+  end
+
+  describe '.extract_issue_id' do
+    subject { RedmineGithub::PullRequestHandler.extract_issue_id(branch_name) }
+
+    context 'when branch_name has an @{issue_id}' do
+      let(:branch_name) { 'feature/@1234-my_first_pr' }
+      it { is_expected.to eq 1234 }
+    end
+
+    context 'when branch_name has a number without @' do
+      let(:branch_name) { 'feature/1234-my_first_pr' }
+      it { is_expected.to be_nil }
     end
   end
 end
