e4e077cdf8160596e7682b9cce17bb6a1ae5f3c5
maimai77 <maimai77@agileware.jp>
2019-04-26 18:33:16 +0900

    refactoring for real GH requests (#22)

--- app/controllers/redmine_github/webhooks_controller.rb
+++ app/controllers/redmine_github/webhooks_controller.rb
@@ -2,9 +2,11 @@
 
 module RedmineGithub
   class WebhooksController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def dispatch_event
       if request.headers['x-github-event'] == 'pull_request'
-        PullRequestHandler.handle(request.body)
+        PullRequestHandler.handle('pull_request' => params[:pull_request])
         head :ok
       else
         head :bad_request
