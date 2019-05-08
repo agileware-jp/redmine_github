b7e38273ffc37ea1649a09cd281e14cf568784ea
tanik <tanimoto@agileware.jp>
2019-05-08 12:13:50 +0900

    Update merged_at field on receive hook

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class PullRequest < ActiveRecord::Base
+  belongs_to :issue
   validates :url, presence: true
 end
--- app/models/redmine_github/pull_request_handler.rb
+++ app/models/redmine_github/pull_request_handler.rb
@@ -8,9 +8,15 @@ module RedmineGithub
       issue_id = extract_issue_id(payload.dig('pull_request', 'head', 'ref'))
       return if issue_id.blank?
 
-      Issue.find_by(id: issue_id)&.create_pull_request(
+      issue = Issue.find_by(id: issue_id)
+      return if issue.blank?
+
+      pull_request = PullRequest.find_or_create_by(
+        issue: issue,
         url: payload.dig('pull_request', 'html_url')
       )
+
+      pull_request.update(merged_at: payload.dig('pull_request', 'merged_at'))
     end
 
     def extract_issue_id(branch_name)
