7b8cd8ad259fa0240777ceba7476984d5bd5f09d
tanik <tanimoto@agileware.jp>
2019-05-08 13:02:41 +0900

    Add icon for merge

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -3,4 +3,12 @@
 class PullRequest < ActiveRecord::Base
   belongs_to :issue
   validates :url, presence: true
+
+  def state
+    if merged_at.present?
+      :merged
+    else
+      :opened
+    end
+  end
 end
