bd06204f5103a0f3ea068ca0f4427e594788023f
tanik <tanimoto@agileware.jp>
2019-05-09 15:24:03 +0900

    Add event handler for push, pull_request_review, and status

--- app/models/redmine_github/pull_request_handler.rb
+++ app/models/redmine_github/pull_request_handler.rb
@@ -4,11 +4,19 @@ module RedmineGithub
   module PullRequestHandler
     module_function
 
-    def handle(payload)
-      issue_id = extract_issue_id(payload.dig('pull_request', 'head', 'ref'))
-      return if issue_id.blank?
+    def handle(event, payload)
+      send("handle_#{event}", payload)
+    end
 
-      issue = Issue.find_by(id: issue_id)
+    def extract_issue_id(branch_name)
+      match = branch_name.to_s.match(/@(\d+)/)
+      return nil unless match
+
+      match.captures[0].to_i
+    end
+
+    def handle_pull_request(payload)
+      issue = Issue.find_by(id: extract_issue_id(payload.dig('pull_request', 'head', 'ref')))
       return if issue.blank?
 
       pull_request = PullRequest.find_or_create_by(
@@ -19,11 +27,29 @@ module RedmineGithub
       pull_request.update(merged_at: payload.dig('pull_request', 'merged_at'))
     end
 
-    def extract_issue_id(branch_name)
-      match = branch_name.to_s.match(/@(\d+)/)
-      return nil unless match
+    def pull_request_review(payload)
+      issue = Issue.find_by(id: extract_issue_id(payload.dig('pull_request', 'head', 'ref')))
+      return if issue.blank?
 
-      match.captures[0].to_i
+      pull_request = PullRequest.find_or_create_by(
+        issue: issue,
+        url: payload.dig('pull_request', 'html_url')
+      )
+
+      pull_request.sync
+    end
+
+    def handle_push(payload)
+      issue = Issue.find_by(id: extract_issue_id(payload.dig('ref')))
+      return if issue.blank?
+
+      pull_request = PullRequest.find_by(issue: issue)
+      pull_request.try!(:sync)
+    end
+
+    def handle_status(payload)
+      issue_ids = payload.dig('branches').map { |b| extract_issue_id(b[:name]) }.compact.uniq
+      PullRequest.where(issue_id: issue_ids).find_each(&:sync)
     end
   end
 end
