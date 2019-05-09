b40ef62f8da94939794e48c1ae3e2c62801cf196
tanik <tanimoto@agileware.jp>
2019-05-09 12:48:06 +0900

    Sync mergeable state

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -11,4 +11,38 @@ class PullRequest < ActiveRecord::Base
       :opened
     end
   end
+
+  def number
+    scan_url if @number.nil?
+    @number
+  end
+
+  def repo_owner
+    scan_url if @repo_owner.nil?
+    @repo_owner
+  end
+
+  def repo_name
+    scan_url if @repo_name.nil?
+    @repo_name
+  end
+
+  def repository
+    @repository ||= Repository::Github.find_by(url: "https://github.com/#{repo_owner}/#{repo_name}.git")
+  end
+
+  def sync
+    return unless repository
+
+    ret = RedmineGithub::GithubAPI.client_by_repository(repository).fetch_pull_request(self)
+    return if ret.errors.any? # TODO logging
+
+    update(mergeable_state: ret.data.repository.pull_request.merge_state_status)
+  end
+
+  private
+
+  def scan_url
+    @repo_owner, @repo_name, @number = url.to_s.scan(/\Ahttps:\/\/github.com\/(.+)\/(.+)\/pull\/(\d+)\z/).flatten
+  end
 end
