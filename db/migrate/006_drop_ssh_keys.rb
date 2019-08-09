78156f0d001410568d03a56037c6a03e99f1fdee
fkdkent <fkdkent@agileware.jp>
2019-08-09 17:27:05 +0900

    Fix typo...

--- db/migrate/006_drop_ssh_keys.rb
+++ db/migrate/006_drop_ssh_keys.rb
@@ -1,7 +1,7 @@
 class DropSshKeys < RedmineGithub::Migration::MIGRATION_CLASS
   # Drops the table on applying and creates the table on rollbacking.
   def change
-    drop :ssh_keys do |t|
+    drop_table :ssh_keys do |t|
       t.belongs_to :user, index: true, foreign_key: true
 
       t.text :public_key
