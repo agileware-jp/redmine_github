0cd034dda2ea503e6a05b9f2565ff2fd36d33041
maimai77 <kohta0707@gmail.com>
2022-06-10 18:03:39 +0900

    support for Redmine5 (#45)

--- lib/redmine_github/reloader.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RedmineGithub
-  Reloader = if RedmineGithub.rails5_or_later?
-               ActiveSupport::Reloader
-             else
-               ActionDispatch::Reloader
-             end
-end
