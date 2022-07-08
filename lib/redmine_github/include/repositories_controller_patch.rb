de52321760daccb50d0c303c944e936a3a05a37f
Yuya.Nishida <yuya@j96.org>
2022-07-08 17:51:32 +0900

    git dust commit [ci skip]

--- lib/redmine_github/include/repositories_controller_patch.rb
+++ lib/redmine_github/include/repositories_controller_patch.rb
@@ -6,10 +6,13 @@ module RedmineGithub
     module RepositoriesControllerPatch
       extend ActiveSupport::Concern
 
+      class WebhookCreationError < StandardError
+      end
+
       included do
         helper GithubHelper
 
-        after_action :create_github_webhook_if_needed, only: :create
+        around_action :show_error_if_web_hook_creation_is_failure, only: :create
       end
 
       private
@@ -19,6 +22,22 @@ module RedmineGithub
         return redmine_github_webhook_url(repository_id: @repository) unless use_hostname == '1'
         webhook_path = redmine_github_webhook_path(repository_id: @repository)
         "#{::Setting.protocol}://#{::Setting.host_name}#{webhook_path}"
+      end
+
+      def show_error_if_web_hook_creation_is_failure
+        ActiveRecord::Base.transaction do
+          yield
+
+          begin
+            create_github_web_hook_if_needed
+          rescue => e
+            logger.error("#{e.class}: #{e.message}")
+            logger.debug { "backtrace:\n#{e.backtrace.join("\n")}" }
+            raise WebhookCreationError
+          end
+        rescue WebhookCreationError
+          
+        end
       end
 
       def create_github_webhook_if_needed
